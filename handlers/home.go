afb91abe169d2eace1e642f4f7f346061a9cf0d5
Brandon Philips <brandon@ifup.co>
2014-02-07 16:32:09 -0800

    fix(handlers): use 301 on home redirect

--- handlers/home.go
+++ handlers/home.go
@@ -8,6 +8,6 @@ func HomeHandler(w http.ResponseWriter, r *http.Request) {
 
 	http.Redirect(w, r,
 		"https://coreos.com/docs/cluster-management/setup/etcd-cluster-discovery/",
-		http.StatusTemporaryRedirect,
+		http.StatusMovedPermanently,
 	)
 }
