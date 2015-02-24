61804c87a9b1b9167b1aafe2d53878f5ef3df95b
Jed Smith <jed@coreos.com>
2015-02-24 12:27:40 -0800

    new: Trim slashes from passed host parameter
    Prevent a double-slash case with the relatively simple fix.

--- handlers/new.go
+++ handlers/new.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"bytes"
 	"crypto/rand"
 	"encoding/hex"
 	"errors"
@@ -81,5 +82,5 @@ func NewTokenHandler(w http.ResponseWriter, r *http.Request) {
 
 	log.Println("New cluster created", token)
 
-	fmt.Fprintf(w, "%s/%s", *baseURI, token)
+	fmt.Fprintf(w, "%s/%s", bytes.TrimRight([]byte(*baseURI), "/"), token)
 }
